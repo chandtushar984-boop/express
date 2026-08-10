740dfd9cfac96492f6b2599988bb42fcbbeeb5c8
TusharEXE <4du2mb0@gmail.com>
2026-08-10 14:00:07 +0530

    nee

---
+++ server.js
@@ -0,0 +1,18 @@
+// const fs = require("fs");
+
+// // fs.writeFile("hello.txt","hello",(err)=>{
+// //        if(err) return err
+// //     console.log("its working......")
+// // })
+
+// fs.appendFile("hello.txt","bhencho gand fatwa lo",(err)=>{
+//     if(err) return err
+//     console.log("it's done")d
+// })  os module path module 
+
+const http = require("http");
+const server = http.createServer((req,res)=>{
+
+})
+
+server.listen(3000);
